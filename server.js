dfd03a4bc96507b01950327f2d475d6c42477728
DESKTOP-8HH9KG6\park <gr1993@naver.com>
2021-10-21 09:22:19 +0900

    create 통합 테스트 구현

--- server.js
+++ server.js
@@ -14,7 +14,20 @@ mongoose.connect('mongodb://localhost:27017/testdb', {
 const app = express();
 const productRoutes = require('./routes');
 
+//미들웨어
 app.use(express.json());
 app.use("/api/product", productRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//미들웨어 동기상황에서 에러가 나면 이곳으로 바로 들어 오지만
+//비동기 상황일 경우 next함수의 인자로 Error를 보내야 여기로옴
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message });
+})
+
 app.listen(PORT);
+
+module.exports = app;
